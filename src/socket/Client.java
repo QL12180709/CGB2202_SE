7f99460fa19c8df61bd2924a51835efea6cf7f08
QL12180709 <3100610545@qq.com>
2022-03-31 15:56:45 +0800

    第一次提交

--- src/socket/Client.java
+++ src/socket/Client.java
@@ -3,13 +3,14 @@ package socket;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /*
  * 聊天室客户端
  *
  * */
 public class Client {
-    /*
+    /*0.0000000000000000000000000000000000000000000000000000000000000000000
      *  java.net.Socket
      *  Socket封装了TCP协议的通讯细节，使用它可以和服务端建立TCP连接，并基于两个流的
      *  读写完成数据交换。
@@ -35,7 +36,9 @@ public class Client {
 
 //            本机IP地址：localhost
             System.out.println("正在连接服务端...");
-            socket = new Socket("localhost", 8088);
+            socket = new Socket("10.1.188.86", 8088);
+//            socket = new Socket("10.1.188.98", 8088);
+//            socket = new Socket("10.1.188.103", 8080);
             System.out.println("与服务端建立连接了！");
         } catch (IOException e) {
             e.printStackTrace();
@@ -48,15 +51,30 @@ public class Client {
     public void start() {
         try {
             OutputStream out = socket.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+            OutputStreamWriter osw = new OutputStreamWriter(out, StandardCharsets.UTF_8);//转换流
             BufferedWriter bw = new BufferedWriter(osw);
             PrintWriter pw = new PrintWriter(bw, true);
 
-
-            pw.println("你好服务端！");
-
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String s = scanner.nextLine();
+                if ("exit".equalsIgnoreCase(s)) {
+                    break;
+                }
+                pw.println(s);
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            try {
+                /*
+                * Socket提供了close()方法，可以与远端计算机断开连接。
+                * 该方法调用时，也会自动关闭通过它获取的输入流和输出流。
+                * */
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
--- src/socket/Server.java
+++ src/socket/Server.java
@@ -1,7 +1,9 @@
 package socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,12 +13,12 @@ public class Server {
      * ServerSocket是运行在服务端上的。其主要有两个作用
      * 1:向服务端申请服务端口(客户端Socket就是通过这个端口与服务端建立连接的)
      * 2:监听服务端口，一旦客户端连接会立即常见一个Socket，通过该Socket与客户端交互
-     *
+     * <p>
      * 如果我们将Socket比喻为"电话"，那么ServerSocket相当于"总机"
      */
     private ServerSocket serverSocket;
 
-    public Server(){
+    public Server() {
         try {
             /*
                 ServerSocket在创建的时候要申请一个固定的端口号，客户端才能通过这个
@@ -25,25 +27,50 @@ public class Server {
                 java.net.BindException:address already in use
                 绑定异常:地址被使用了
              */
+            System.out.println("正在启动服务端...");
             serverSocket = new ServerSocket(8088);
+            System.out.println("服务端启动完毕!");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void start(){
+    public void start() {
+
         try {
             /*
-            * ServerSocket的accept方法是一个阻塞方法。
-            * 开始等待客户端的连接，一旦一个客户端通过端口建立连接，此时accept方法相当于是接电话的动作。
-            * 阻塞方法：调用后，程序就“卡住”不动了
-            *
-            * */
+             * ServerSocket的accept方法是一个阻塞方法。
+             * 开始等待客户端的连接，一旦一个客户端通过端口建立连接，此时accept方法相当于是接电话的动作。
+             * 阻塞方法：调用后，程序就“卡住”不动了
+             *
+             * */
+
             System.out.println("等待客户端连接...");
-            Socket socket = serverSocket.accept();
-            System.out.println("一个客户端连接了！");
+            int count = 0;
+            while (true) {
+                Socket socket = serverSocket.accept();
+                count++;
+                System.out.println(count + "个客户端连接了！");
 
 
+//            通过socket获取输入流读取对方发送过来的消息
+                InputStream in = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(in);
+                BufferedReader br = new BufferedReader(isr);
+                String s = null;
+                /*
+                 * 这里的BufferedReader读取时低下连接的流是通过Socket获取的输入流
+                 * 当远端计算机还处于连接状态，但是暂时没有发送内容，readLine方法会
+                 *  处于阻塞状态，直到对方发送过来一行字符串为止。
+                 *  如果返回值为null，则表示对方断开了连接(对方调用了socket.close())。
+                 *
+                 *
+                 * */
+                while ((s = br.readLine()) != null) {
+                    System.out.println("客户端说：" + s);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
