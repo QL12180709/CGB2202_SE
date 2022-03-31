e392cdff77205085e4184e6ec3219757e5420fa4
QL12180709 <3100610545@qq.com>
2022-03-31 09:48:14 +0800

    第一次提交

---
+++ src/socket/Client.java
@@ -0,0 +1,17 @@
+package socket;
+
+import java.net.Socket;
+
+/*
+* 聊天室客户端
+*
+* */
+public class Client {
+    /*
+    *  java.net.Socket
+    *  Socket封装了TCP协议的通讯细节，使用它可以和服务端建立TCP连接，并基于两个流的
+    *  读写完成数据交换。
+    *
+    * */
+    private Socket socket;
+}
