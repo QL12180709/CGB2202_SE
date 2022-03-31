60c71b238283380e3a4067e0c9389a0f1235ea33
QL12180709 <3100610545@qq.com>
2022-03-31 11:51:55 +0800

    第一次提交

--- src/socket/Client.java
+++ src/socket/Client.java
@@ -1,52 +1,67 @@
 package socket;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 /*
-* 聊天室客户端
-*
-* */
+ * 聊天室客户端
+ *
+ * */
 public class Client {
     /*
-    *  java.net.Socket
-    *  Socket封装了TCP协议的通讯细节，使用它可以和服务端建立TCP连接，并基于两个流的
-    *  读写完成数据交换。
-    *
-    * */
+     *  java.net.Socket
+     *  Socket封装了TCP协议的通讯细节，使用它可以和服务端建立TCP连接，并基于两个流的
+     *  读写完成数据交换。
+     *
+     * */
     private Socket socket;
 
 
     /*
-    * 构造方法：用于初始化客户端
-    * */
-    public Client(){
+     * 构造方法：用于初始化客户端
+     * */
+    public Client() {
         try {
             /*
-            * 实例化Socket时常用的构造方法
-            * Socket(String host,int port)
-            * 这个构造器实例化Socket的过程就是与服务器建立连接的过程。
-            * 参数1：服务器的IP地址
-            * 参数2：服务端开启的服务端口
-            * 我们通过服务端的IP可以找到网络上服务端所在的计算机。通过端口号可以找到该机器上的
-            *该机器上的服务端应用程序从而与之建立连接。
-            */
-            socket=new Socket("locahost",8080);
+             * 实例化Socket时常用的构造方法
+             * Socket(String host,int port)
+             * 这个构造器实例化Socket的过程就是与服务器建立连接的过程。
+             * 参数1：服务器的IP地址
+             * 参数2：服务端开启的服务端口
+             * 我们通过服务端的IP可以找到网络上服务端所在的计算机。通过端口号可以找到该机器上的
+             *该机器上的服务端应用程序从而与之建立连接。
+             */
+
+//            本机IP地址：localhost
+            System.out.println("正在连接服务端...");
+            socket = new Socket("localhost", 8088);
+            System.out.println("与服务端建立连接了！");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     /*
-    * 客户端开始的方法
-    * */
-    public void start(){
+     * 客户端开始的方法
+     * */
+    public void start() {
+        try {
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+            BufferedWriter bw = new BufferedWriter(osw);
+            PrintWriter pw = new PrintWriter(bw, true);
 
+
+            pw.println("你好服务端！");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         Client client = new Client();
         client.start();
-
     }
 }
--- src/socket/Server.java
+++ src/socket/Server.java
@@ -1,5 +1,58 @@
 package socket;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    /**
+     * java.net.ServerSocket
+     * ServerSocket是运行在服务端上的。其主要有两个作用
+     * 1:向服务端申请服务端口(客户端Socket就是通过这个端口与服务端建立连接的)
+     * 2:监听服务端口，一旦客户端连接会立即常见一个Socket，通过该Socket与客户端交互
+     *
+     * 如果我们将Socket比喻为"电话"，那么ServerSocket相当于"总机"
+     */
+    private ServerSocket serverSocket;
+
+    public Server(){
+        try {
+            /*
+                ServerSocket在创建的时候要申请一个固定的端口号，客户端才能通过这个
+                端口建立连接。
+                如果该端口被当前操作系统中其他程序使用了，那么这里实例化会抛出异常:
+                java.net.BindException:address already in use
+                绑定异常:地址被使用了
+             */
+            serverSocket = new ServerSocket(8088);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start(){
+        try {
+            /*
+            * ServerSocket的accept方法是一个阻塞方法。
+            * 开始等待客户端的连接，一旦一个客户端通过端口建立连接，此时accept方法相当于是接电话的动作。
+            * 阻塞方法：调用后，程序就“卡住”不动了
+            *
+            * */
+            System.out.println("等待客户端连接...");
+            Socket socket = serverSocket.accept();
+            System.out.println("一个客户端连接了！");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
 
 }
